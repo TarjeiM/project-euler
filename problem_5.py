9ee405d86103beef00c2d728bc6952c360b45e05
TarjeiM <136925780+TarjeiM@users.noreply.github.com>
2025-01-05 19:16:20 +0100

    Update problem_5.py

--- problem_5.py
+++ problem_5.py
@@ -3,9 +3,10 @@
 num = 9699690
 
 def divisible_by_1_through_20(number: int) -> bool:
-    return number % 20 == 0 and number % 19 == 0 and number % 18 == 0 \
-    and number % 17 == 0 and number % 16 == 0 and number % 15 == 0 \
-    and number % 14 == 0 and number % 13 == 0 and number % 12 == 0 and number % 11 == 0
+    for i in range(20,10,-1):
+        if number % i != 0:
+            return False
+    return True
 
 while not divisible_by_1_through_20(num):
     num += 1
