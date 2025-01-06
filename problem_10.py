009ee021fdfb8364993108c390cc663a1266936f
TarjeiM <136925780+TarjeiM@users.noreply.github.com>
2025-01-06 01:12:22 +0100

    Update problem_10.py

--- problem_10.py
+++ problem_10.py
@@ -3,14 +3,11 @@
 def is_prime(number: int) -> bool:
     if number % 2 == 0 or number % 3 == 0:
         return False
-    
     curr, limit = 5, number**0.5
-
     while curr <= limit:
         if number % curr == 0:
             return False
         curr += 2
-    
     return True
 
 total = 5
