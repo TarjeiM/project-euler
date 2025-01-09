9e612a87b4b52659f600c410662856b9b432e5a4
TarjeiM <136925780+TarjeiM@users.noreply.github.com>
2025-01-09 19:28:51 +0100

    Update problem_19_datetime.cs

--- problem_19/problem_19_datetime.cs
+++ problem_19/problem_19_datetime.cs
@@ -2,25 +2,21 @@
 
 using System;
 
-void Main()
-{
-    int res = 0;
-    int day = 1;
 
-    for (int year = 1901; year < 2001; year++)
+int res = 0;
+int day = 1;
+
+for (int year = 1901; year < 2001; year++)
+{
+    for (int month = 1; month < 13; month++)
     {
-        for (int month = 1; month < 13; month++)
+        DateTime dateTime = new DateTime(year,month,day);
+        if ((int)dateTime.DayOfWeek == 0)
         {
-            DateTime dateTime = new DateTime(year,month,day);
-            if ((int)dateTime.DayOfWeek == 0)
-            {
-                res++;
-            }
+            res++;
         }
     }
-
-    Console.WriteLine(res);
 }
 
-Main();
+Console.WriteLine(res);
 
