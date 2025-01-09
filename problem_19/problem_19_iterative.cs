8cfbe7451f20ca8b9da302ce20478dc87fc33f33
TarjeiM <136925780+TarjeiM@users.noreply.github.com>
2025-01-09 19:29:22 +0100

    Update problem_19_iterative.cs

--- problem_19/problem_19_iterative.cs
+++ problem_19/problem_19_iterative.cs
@@ -2,38 +2,35 @@
 
 using System;
 
-void Main()
-{
-    int res = 0;
-    int day = 0;
-    int[] daysInMonth = new int[12] {31,28,31,30,31,30,31,31,30,31,30,31};  
+int res = 0;
+int day = 0;
+int[] daysInMonth = new int[12] {31,28,31,30,31,30,31,31,30,31,30,31};  
 
-    for (int year = 1901; year < 2001; year++)
+for (int year = 1901; year < 2001; year++)
+{
+    bool leap = isLeap(year);
+    for (int month = 1; month < 13; month++)
     {
-        bool leap = isLeap(year);
-        for (int month = 1; month < 13; month++)
+        int days = daysInMonth[month-1];
+        if (month == 2 && leap)
         {
-            int days = daysInMonth[month-1];
-            if (month == 2 && leap)
+            days = 29;
+        }
+        for (int i = 0; i < days; i++)
+        {
+            day = day == 7 ? 1 : day + 1;
+            if (i == 0 && day == 1)
             {
-                days = 29;
-            }
-            for (int i = 0; i < days; i++)
-            {
-                day = day == 7 ? 1 : day + 1;
-                if (i == 0 && day == 1)
-                {
-                    res++;
-                }
+                res++;
             }
         }
     }
-    Console.WriteLine(res);
 }
+Console.WriteLine(res);
 
 bool isLeap(int year)
 {
     return year % 4 == 0;
 }
 
-Main();
+
