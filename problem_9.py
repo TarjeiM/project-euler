9695e5263426e55f4249e51b4787d84fd73a48e8
TarjeiM <136925780+TarjeiM@users.noreply.github.com>
2025-11-02 19:41:43 +0100

    Delete problem_9.py

--- problem_9.py
+++
@@ -1,8 +0,0 @@
-# Special Pythagorean Triplet
-
-for a in range(1,500):
-    for b in range(a+1,500):
-        c = (a**2 + b**2)**0.5
-        if a + b + c == 1000:
-            print(f"{a*b*c:.0f}")
-            break
