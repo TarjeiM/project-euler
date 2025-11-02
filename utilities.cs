4ef9c64b5403682cb606ff8ecc57cc04a0684d23
TarjeiM <136925780+TarjeiM@users.noreply.github.com>
2025-11-02 21:25:26 +0100

    Indentation fix

--- utilities.cs
+++ utilities.cs
@@ -2,20 +2,21 @@
 namespace csharp.utilities
 {
     public static class Utilities
-{
-    public static int[] Primes(int limit)
     {
-        List<int> primes = new();
-        int[] numbers = new int[limit + 1];
-        for (int i = 2; i < limit + 1; i++)
+        public static int[] Primes(int limit)
         {
-            if (numbers[i] == 0) { primes.Add(i); }
-            for (int j = i * 2; j < limit + 1; j += i)
+            List<int> primes = new();
+            int[] numbers = new int[limit + 1];
+            for (int i = 2; i < limit + 1; i++)
             {
-                numbers[j] = 1;
+                if (numbers[i] == 0) { primes.Add(i); }
+                for (int j = i * 2; j < limit + 1; j += i)
+                {
+                    numbers[j] = 1;
+                }
             }
+            return [.. primes];
         }
-        return [.. primes];
     }
 }
-}
+
